1396412333807384eba77a4ec09f3477b2c35473
robnagler <github@q33.us>
2017-09-30 23:26:36 +0000

    run redhat-base as root

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -2,8 +2,11 @@
 build_fedora_base_image
 build_travis_trigger_next=( beamsim-part1 )
 
+build_as_root() {
+    curl radia.run | bash -s redhat-base
+}
+
 build_as_run_user() {
     cd
-    curl radia.run | bash -s redhat-base
     bivio_pyenv_2
 }
