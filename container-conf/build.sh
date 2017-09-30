e2bf5918ea6c8c2733c361372ed51fb1ea0edb3b
robnagler <github@q33.us>
2017-09-30 21:54:24 +0000

    move rpms to redhat-base

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,13 +3,7 @@ build_fedora_base_image
 build_travis_trigger_next=( beamsim-part1 )
 
 build_as_run_user() {
-    if [[ $build_is_vagrant ]]; then
-        build_sudo rpm --import https://yum.dockerproject.org/gpg
-        build_sudo cp docker.repo /etc/yum.repos.d/docker.repo
-        build_yum install docker-engine
-        build_sudo usermod -a -G docker vagrant
-        build_sudo systemctl enable docker.service
-    fi
     cd
+    curl radia.run | bash -s redhat-base
     bivio_pyenv_2
 }
