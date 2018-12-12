55adfdf1a1edad21d80b8a58fa7ecf74a52dbc60
robnagler <github@q33.us>
2018-12-12 23:42:26 +0000

    simplified to just python2

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,12 +4,9 @@ build_is_public=1
 
 build_as_root() {
     install_repo_eval redhat-base
-    install_repo_eval code-base
 }
 
 build_as_run_user() {
     cd
     bivio_pyenv_2
-    gem install --no-document fpm
-    build_run_user_home_chmod_public
 }
