25c1a30bc3968241f219dcaf1e9a4c88e84732ee
Evan Carlin <evan@carlin.com>
2022-04-15 20:39:28 +0000

    x

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,5 +4,8 @@ build_is_public=1
 
 build_as_run_user() {
     umask 022
-    bivio_pyenv_3
+    # bivio_pyenv_3
+    _bivio_pyenv_global_version='3.10.2'  _bivio_pyenv_install
+    _bivio_pyenv_version_do 3.10.2 py3
+
 }
